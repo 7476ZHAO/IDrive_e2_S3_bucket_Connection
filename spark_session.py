71d5c0144eaf3ae38e1f015e0187b63faf872675
Jiali Zhao <7476zhao@gmail.com>
2026-04-21 13:23:42 -0500

    Update spark_session.py

--- spark_session.py
+++ spark_session.py
@@ -1,14 +1,37 @@
 from pyspark.sql import SparkSession
 
 def get_spark():
-    return SparkSession.builder
+    return (
+        SparkSession.builder
         .appName("delta-test")
         .config("spark.jars.packages",
                 "org.apache.hadoop:hadoop-aws:3.3.1,io.delta:delta-core_2.12:2.4.0")
         .config("spark.hadoop.fs.s3a.endpoint",
-                "https://s3.us-west-1.idrivee2.com") # need to be updated  
+                "https://s3.us-west-1.idrivee2.com")
         .config("spark.hadoop.fs.s3a.path.style.access", "true")
         .config("spark.hadoop.fs.s3a.aws.credentials.provider",
                 "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
         .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "true")
         .getOrCreate()
+    )
+
+
+# used for sts
+# from pyspark.sql import SparkSession
+# import os
+
+# def get_spark():
+#     return SparkSession.builder \
+#         .appName("delta-test") \
+#         .config("spark.jars.packages",
+#                 "org.apache.hadoop:hadoop-aws:3.3.1,io.delta:delta-core_2.12:2.4.0") \
+#         .config("spark.hadoop.fs.s3a.endpoint",
+#                 "https://your-endpoint.idrivee2.com") \
+#         .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+#         .config("spark.hadoop.fs.s3a.access.key",
+#                 os.getenv("AWS_ACCESS_KEY_ID")) \
+#         .config("spark.hadoop.fs.s3a.secret.key",
+#                 os.getenv("AWS_SECRET_ACCESS_KEY")) \
+#         .config("spark.hadoop.fs.s3a.session.token",
+#                 os.getenv("AWS_SESSION_TOKEN")) \
+#         .getOrCreate()
