c33b53c23313de977a85118d33911c430fb0b07a
Jiali Zhao <7476zhao@gmail.com>
2026-04-22 09:09:39 -0500

    Update spark_session.py

--- spark_session.py
+++ spark_session.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from config import S3_ENDPOINT
 
 def get_spark():
     return (
@@ -7,7 +8,7 @@ def get_spark():
         .config("spark.jars.packages",
                 "org.apache.hadoop:hadoop-aws:3.3.1,io.delta:delta-core_2.12:2.4.0")
         .config("spark.hadoop.fs.s3a.endpoint",
-                "https://s3.us-midwest-1.idrivee2.com")
+                S3_ENDPOINT)
         .config("spark.hadoop.fs.s3a.path.style.access", "true")
         .config("spark.hadoop.fs.s3a.aws.credentials.provider",
                 "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
