134906eec591280d2b079d6a11cee55ff9cda5ca
Jiali Zhao <7476zhao@gmail.com>
2026-04-22 13:59:07 -0500

    Update test_s3_connection.py

--- test_s3_connection.py
+++ test_s3_connection.py
@@ -1,17 +1,18 @@
 from spark_session import get_spark
-from config import DATA_PATH
+from data_io import load_data, save_data
 
 spark = get_spark()
 
-# 1️ Create a local DataFrame
+# 1 Create a local DataFrame
 df = spark.createDataFrame([
     (1, "Alice"),
-    (2, "Sarah")
+    (2, "Bob")
 ], ["id", "name"])
 
-# 2️ Write to S3 (Delta format)
-df.write.format("delta").mode("overwrite").save(DATA_PATH)
+# 2️ Write to S3 using your function
+save_data(df, spark)
 
-# 3️ Read back from S3 to verify
-df2 = spark.read.format("delta").load(DATA_PATH)
+# 3️ Read back using your function
+df2 = load_data(spark)
+
 df2.show()
