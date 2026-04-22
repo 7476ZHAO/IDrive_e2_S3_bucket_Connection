8ca83ef11b46ef063e26bad0da4618dce816bc69
Jiali Zhao <7476zhao@gmail.com>
2026-04-22 14:24:28 -0500

    Update spark_session.py

--- spark_session.py
+++ spark_session.py
@@ -7,8 +7,11 @@ def get_spark():
         .appName("delta-test")
         .config("spark.jars.packages",
                 "org.apache.hadoop:hadoop-aws:3.3.1,io.delta:delta-core_2.12:2.4.0")
-        .config("spark.hadoop.fs.s3a.endpoint",
-                S3_ENDPOINT)
+        .config("spark.sql.extensions",
+                "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog",
+                "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.hadoop.fs.s3a.endpoint", S3_ENDPOINT)
         .config("spark.hadoop.fs.s3a.path.style.access", "true")
         .config("spark.hadoop.fs.s3a.aws.credentials.provider",
                 "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
