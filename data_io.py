4427b63819a747b5badfa1e561f79b3958115b72
Jiali Zhao <7476zhao@gmail.com>
2026-04-29 09:18:28 -0500

    Update data_io.py

--- data_io.py
+++ data_io.py
@@ -4,4 +4,4 @@ def load_data(spark):
     return spark.read.format("delta").load(DATA_PATH)
 
 def save_data(df, spark):
-    df.write.format("delta").mode("overwrite").save(DATA_PATH)
+    df.write.format("delta").mode("append").save(DATA_PATH)
